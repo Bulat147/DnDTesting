94e62e878357e9385f8230a5e5fa08a23d6e36c8
Bulat <ninza147258@gmail.com>
2024-05-21 21:25:55 +0300

    add parser mapper

--- src/test/java/per/khalilov/test/MagicItemTest.java
+++ src/test/java/per/khalilov/test/MagicItemTest.java
@@ -1,18 +1,12 @@
 package per.khalilov.test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import per.khalilov.ApplicationManager;
-import per.khalilov.model.AccountData;
 import org.junit.Test;
 import per.khalilov.model.MagicItemData;
-import per.khalilov.util.Settings;
 
 import java.util.List;
-import java.util.UUID;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -49,14 +43,6 @@ public class MagicItemTest extends AuthBase {
 
     @Test
     public void testEditMagicItem() {
-        MagicItemData magicItem = new MagicItemData(UUID.randomUUID().toString(),
-                MagicItemData.ItemTypeEnum.Potion,
-                MagicItemData.ItemQualityEnum.Artefact,
-                "safsdgfsdfsgsdfgdsf g nfg fdn dkfj jdfs,b df.kjb dhfj bkjdfb ksjdb hjbcxj bsdb kjb jhb jkdb cxjkb kdsjbxcjkxb jds");
-        magicItem.setPrice("65436");
-        magicItem.setSubtype("sdfgsfdfsdvfsd");
-        magicItem.setSubquality("sgsdfsd");
-        magicItem.setTitleEn("sdfasdgsdfvf");
         String magicItemEditedSubtype = "testSubtypeeeeeeeee";
 
         manager.magicItem().createMagicItem(magicItem);
